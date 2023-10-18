e64e55e7b88d0bdccb301244532b59f149c2c6a0
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-10-17 21:07:25 -0600

    feat(common): 🔨 Add the 'NavBar.tsx' component

---
+++ src/common/NavBar.tsx
@@ -0,0 +1,34 @@
+import { 
+  Box,
+  AppBar,
+  Toolbar,
+  Container,
+  Grid,
+  Button,
+  Typography,
+  Stack
+} from '@mui/material'
+
+export const NavBar: React.FC = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='fixed'>
+        <Toolbar>
+          <Container maxWidth='xl'>
+            <Grid container direction='row' justifyContent='space-between' alignItems='center'>
+              <Grid item>
+                <Typography>Josue</Typography>
+              </Grid>
+              <Grid item>
+                <Stack spacing={2} direction='row'>
+                  <Button variant='contained'>Login</Button>
+                  <Button variant='outlined'>Register</Button>
+                </Stack>
+              </Grid>
+            </Grid>
+          </Container>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
