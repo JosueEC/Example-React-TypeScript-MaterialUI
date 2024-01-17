d3c33aaa66f9c6f54e57cb3777a5a8b19e114a6b
J Evangelista Cruz <josueev.cruz@gmail.com>
2024-01-16 20:14:09 -0600

    feat(pages/Home): 🔨 Retrieve and map the characters array from the API

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,64 @@
-import { Container, Button } from '@mui/material'
-import { useNotification } from '../../context/notification'
+import { useEffect, FC, useState } from 'react';
+import { Container, Button, Grid } from '@mui/material'
+import { CardComponent, HeaderComponent } from '../../components'
+import { characters } from '../../api/characters';
+import { Character } from './interfaces/character.interface';
 
-export const HomePage: React.FC = () => {
-  const { getError } = useNotification();
+export const HomePage: FC = () => {
+  const [allCharacters, setAllCharacters] = useState<Character[] | null>(null);
 
-  const handleClick = () => {
-    getError('Hola Mundo')
-  }
+  useEffect(() => {
+    /**
+     * Esta es la forma en la que podemos hacer uso de las funciones
+     * definidas en el apartado 'api'
+     * 
+     * En este caso estamos usanod la funcion findAll de characters,
+     * esta se maneja como una promesa, ya que al ser una conexion
+     * con la API es una accion asincronica
+     */
+    characters.findAll({})
+      .then((response) => {
+        setAllCharacters(response.data.results);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }, []);
 
   return (
-    <Container sx={{ mt: 9 }} maxWidth="xl">
-      <Button variant='contained' onClick={handleClick}>Home</Button>
+    <Container maxWidth="xl">
+      <HeaderComponent
+        title='Hola Mundo'
+        description='Bienvenido a mi chamba'
+        element={<Button variant='contained'>Soy un boton</Button>}
+      />
+      <div>
+        {
+          (allCharacters?.length !== 0) ? (
+            <Grid
+              container
+              spacing={2}
+              direction='row'
+            >
+              {
+                allCharacters?.map((character) => (
+                  <Grid item xs={ 3 }>
+                    <CardComponent
+                      key={ character.id }
+                      name={ character.name }
+                      image={ character.image }
+                      species={ character.species }
+                      status={ character.status }
+                    />
+                  </Grid>
+                ))
+              }
+            </Grid>
+          ) : (
+            <div>No existe data</div>
+          )
+        }
+      </div>
     </Container>
   )
 }
