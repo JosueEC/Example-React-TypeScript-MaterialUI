0b9ca0b8dc8a4a9ad092b4e8e36941ab5aa70c43
J Evangelista Cruz <josueev.cruz@gmail.com>
2024-01-17 13:02:37 -0600

    docs(RouterLayout.tsx): 💡 Add descriptive comments for the component

--- src/common/RouterLayout.tsx
+++ src/common/RouterLayout.tsx
@@ -1,13 +1,23 @@
 import { Outlet } from "react-router-dom"
 import { NavBar } from "./NavBar"
 
+/**
+ * * <Outlet />
+ * Este componente de react-router-dom nos sivre para agrupar rutas
+ * y compartir componentes entre el grupo. Aqui estaremos
+ * compartiendo el componente <NavBar /> entre las rutas '/' y
+ * 'login'.
+ * 
+ * Esto significa que cuando navegemos hacia Home o hacia Login
+ * en ambas paginas se mostrara la NavBar
+ * 
+ * Este componente trabaja en combinacion con una anidacion en el
+ * archivo router.tsx
+ */
 export const RouterLayout: React.FC = () => {
   return (
     <div>
       <NavBar />
-      {/* La etiqueta outlet nos sirve para agrupar rutas y compartir
-      entre componentes, aqui estaremos compartiendo la NavBar entre
-      las rutas '/' y 'login' */}
       <Outlet />
     </div>
   )
