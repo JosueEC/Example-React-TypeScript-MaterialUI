203ae41dcdb44f52455d4b135b7eb47b6bf437cd
J Evangelista Cruz <josueev.cruz@gmail.com>
2024-01-17 13:00:29 -0600

    feat(router.tsx): 🔨 Add descriptive comments for the RouterLayout component and add the '/character/:characterId' route

--- src/router.tsx
+++ src/router.tsx
@@ -2,20 +2,35 @@ import { Routes, Route } from "react-router-dom"
 import { HomePage } from "./pages/home"
 import { LoginPage } from "./pages/login"
 import { RouterLayout } from "./common/RouterLayout"
+import { CharacterPage } from "./pages/character"
 
+/**
+ * * <Route path='/' element={ <RouterLayout /> }>
+ * *  ...
+ * * </Route>
+ * Esta es la forma en la que podemos agrupar rutas en nuestra
+ * aplicacion. Esto trabaja en conjunto con el componente
+ * <Outlet /> en el RouterLayout.tsx
+ * 
+ * 1.- Basicamente abrimos y cerramos un componente Route de
+ * react-router-dom.
+ * 
+ * 2.- En la parte element definimos el componente
+ * que agrupara las rutas (previemente este componente debe estar
+ * usando el componente Outlet en su estructura).
+ * 
+ * 3.- Ahora, dentro de esta ruta podemos definir cuantas rutas
+ * deseemos, todas estas rutas seran representadas en el Outlet
+ * de nuestro RouterLayout, por lo que todos los componentes que
+ * esten definidos en este seran compoartidos y visibles en las
+ * rutas que colocamos dentro de la anidacion.
+ */
 export const AppRouter: React.FC = () => {
   return (
     <Routes>
       <Route path='/' element={<RouterLayout />}>
-        {/* Esta es la forma en la que agruapamps rutas
-        estas rutas seran recibidas en el outlet del
-        componente RouterLayout. De esta forma, todos
-        los componentes que esten en RouterLayout
-        tambien se mostraran en la ruta Home y Login
-        y todas las rutas que esten por fuera de esta
-        anidada no mostrarn los componentes que tiene
-        RouterLayout */}
         <Route path="/" element={<HomePage />} />
+        <Route path="/character/:characterId" element={<CharacterPage />} />
       </Route>
       <Route path="/login" element={<LoginPage />} />
     </Routes>
