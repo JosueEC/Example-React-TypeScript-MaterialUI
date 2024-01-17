550b87db25891faaf582edf62463aeadf9b6f2e0
J Evangelista Cruz <josueev.cruz@gmail.com>
2024-01-16 20:09:02 -0600

    docs(App.tsx): 💡 Add descriptive comments for the NotificationProvider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRouter } from './router'
-import { NotificationProvider } from './context/notification'
+import { NotificationProvider } from './context/notification.context'
 
+/**
+ * * <NotificationProvider>
+ * Es el componente que creamos en nuestro context, el cual nos
+ * permite dar acceso al contexto a los componentes que estan
+ * dentro del componente NotificationProvider, que en este caso
+ * es toda la aplicacion
+ */
 function App() {
   return (
     <div>
